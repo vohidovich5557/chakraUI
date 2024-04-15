e429743621e6a15d8a0d00204c6a333eb7607b07
vohidovich5557 <shokhuk006@gmail.com>
2024-04-15 19:00:09 +0500

    chakra

--- src/config/request.js
+++ src/config/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const request = axios.create({
-  baseURL: "https://chakra-server-tau.vercel.app/",
+  baseURL: "https://chakra-server-kappa.vercel.app/",
 });
 
 export { request };
